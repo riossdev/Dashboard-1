d2481fbad77088c21f8d40ece0c6f06977dbc409
Juan Rios <jriossdev@gmail.com>
2023-05-05 08:21:26 -0500

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() { 
+  return (
+    <div className="text-center bg-indigo-600 text-4xl text-white font-extrabold py-4">
+      <h1>Hello Word!</h1>
+    </div>
+    
+  )
+}
+export default App
